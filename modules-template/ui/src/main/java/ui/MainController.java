e2bbbf4cb97a6fc5161477c2450641dda1e45da6
synnemoe <synnemoe@dhcp-10-220-214-231.wlan.ntnu.no>
2021-09-23 16:28:53 +0200

    Made Controller classes and FXML-files

---
+++ modules-template/ui/src/main/java/ui/MainController.java
@@ -0,0 +1,15 @@
+package ui;
+
+//husk å importere logikk
+
+public class MainController {
+
+	//opprette et objekt av det vi skal ha
+	//pushe samme objekt som ble opprettet i dashboardet over i main
+	private Main main;
+	
+	public void sendMain(Main main) {
+		this.main = main;
+	}
+	
+}
