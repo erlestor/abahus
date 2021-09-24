270c43f09d30ce15beaf83bd0c18bb3dd2a86419
Andreas Netteland <andreas@disputas.no>
2021-09-24 09:07:00 +0200

    add json class

---
+++ modules-template/core/src/main/java/core/Json.java
@@ -0,0 +1,12 @@
+package core;
+
+// import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Json {
+
+    // private static ObjectMapper mapper;
+
+    // private static ObjectMapper getDefaultObjectMapper() {
+    //     ObjectMapper defaultObjectMapper = new ObjectMapper();
+    // }
+}
