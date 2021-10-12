7a686354201e79c8fd8683fe96121e2c13c9f493
Erlend Storsve <erlestor@stud.ntnu.no>
2021-10-12 17:00:53 +0200

    houses are now shown to user

--- housing/ui/src/main/java/ui/MainController.java
+++ housing/ui/src/main/java/ui/MainController.java
@@ -2,16 +2,40 @@ package ui;
 
 import core.*;
 
+import java.io.IOException;
+import java.util.Collection;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
 
 public class MainController {
 
-	@FXML public Label email, listHouse;
+	@FXML public Label email, listHouse, myHousesList;
 	private Main main;
+
+	@FXML
+    public void showAvailableHousing() {
+		listHouse.setText(main.getHousesAsString(main.getAvailableHousing()));
+    }
+
+	@FXML
+    public void showMyHouses() {
+		System.out.println("initializing MainController");
+		if (main != null) {
+			myHousesList.setText(main.getHousesAsString(main.getMyHouses()));
+		}
+    }
 	
 	public void sendMain(Main main) {
 		this.main = main;
+		showAvailableHousing();
+		showMyHouses();
 	}
 	public void setEmail(String email) {
 		this.email.setText(email);
