a746b89667077b9e6a944b01489d60d7dec5594b
Hilde Mikaelsen <hildmi@stud.ntnu.no>
2021-10-13 09:55:53 +0000

    UserTest update

--- housing/core/src/test/java/core/UserTest.java
+++ housing/core/src/test/java/core/UserTest.java
@@ -11,5 +11,20 @@ public class UserTest {
         user = new User("mail@mail.com", "123");
         assertEquals("mail@mail.com", user.getEmail());
         assertEquals("123", user.getPassword());
+
+        assertThrows(IllegalArgumentException.class, () -> {New User (null, "");});
+        assertThrows(IllegalArgumentException.class, () -> {New User (null, "123");});
+        assertThrows(IllegalArgumentException.class, () -> {New User("mail@mail.com", null);});
+        
+    }
+
+    @Test
+    public void testInvalidEmail(){
+
+        assertThrows(IllegalArgumentException.class, () -> {New User("email", "123");});
+        assertThrows(IllegalArgumentException.class, () -> {New User("e@e", "123");});
+        assertThrows(IllegalArgumentException.class, () -> {New User("e.no", "123");});
+        assertThrows(IllegalArgumentException.class, () -> {New User("æøå@gmail.com", "123");});
+
     }
 }
