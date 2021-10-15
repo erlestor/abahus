636c5bd3a5eaf2ea5b3e0c76d494ad240abefbd2
Andreas Netteland <andreas@disputas.no>
2021-10-15 14:38:29 +0200

    update tests to fit new namespaces

--- housing/core/src/test/java/core/HouseTest.java
+++ housing/core/src/test/java/core/HouseTest.java
@@ -1,4 +1,5 @@
 package core;
+import jsonworker.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- housing/core/src/test/java/core/UserTest.java
+++ housing/core/src/test/java/core/UserTest.java
@@ -1,4 +1,5 @@
 package core;
+import jsonworker.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
