04cfa67250aca8bddba4f1fcbab5798722d1c154
Andreas Netteland <andreas@disputas.no>
2021-10-29 09:03:12 +0200

    setup react router and create pages for landing and login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./landing";
+import Landing from "./components/landing";
+import Login from "./components/login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const Component = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <React.StrictMode>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Landing />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<Component />, document.getElementById("root"));
