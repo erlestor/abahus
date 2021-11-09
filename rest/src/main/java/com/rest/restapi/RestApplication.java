01de9ee8e7cb77fcecb442601390ffcd7b92f5de
Hilde Mikaelsen <hildmi@stud.ntnu.no>
2021-11-09 14:29:55 +0000

    Revert "revert commit"
    This reverts commit dbb7b74d2dd1ae6ea651f368f8e79ee2a2a2f32a.

--- rest/src/main/java/com/rest/restapi/RestApplication.java
+++ rest/src/main/java/com/rest/restapi/RestApplication.java
@@ -1,25 +1,31 @@
-package com.example.rest;
+package com.rest.restapi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import core.Main;
+
+
 
 @SpringBootApplication
 @RestController
 public class RestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) throws JsonParseException, JsonMappingException, IOException {
+		Main m = new Main("1@2.com", "123", "123");
+		return String.format("Hello %s!", m.getCurrentUser());
 	}
 
 }
