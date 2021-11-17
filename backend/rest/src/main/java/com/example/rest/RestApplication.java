17b553fb0de3b0eba62cdbdee23caf4926268a81
Synne Frafjord Moe <synnefm@stud.ntnu.no>
2021-11-17 14:07:55 +0000

    made backend pom.file connect to restAPI

--- backend/rest/src/main/java/com/example/rest/RestApplication.java
+++ backend/rest/src/main/java/com/example/rest/RestApplication.java
@@ -1,4 +1,4 @@
-package com.rest.restapi;
+package com.example.rest;
 
 import java.io.IOException;
 
@@ -8,27 +8,18 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import core.Main;
 import core.House;
 import core.User; 
-import jsonworker.Jsonworker; 
-import java.util.ArrayList;
 
 
 @SpringBootApplication
@@ -56,7 +47,6 @@ public class RestApplication {
 		
 	}
 
-	/
 	@ResponseBody @RequestMapping(value="/removeHouse/{location}", method=RequestMethod.GET)
 	public String removeHouse(@PathVariable("location") String location) throws IOException {
 		if (this.m == null){
