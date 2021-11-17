6f787e0f7a5ccd1eb60ace457088e15b3e878350
Hilde Mikaelsen <hildmi@stud.ntnu.no>
2021-11-17 12:22:39 +0000

    removeHouse and addHouse in RestApplication

--- backend/rest/src/main/java/com/example/rest/RestApplication.java
+++ backend/rest/src/main/java/com/example/rest/RestApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,6 +36,7 @@ import java.util.ArrayList;
 public class RestApplication {
 
 	private Jsonworker jsonworker; 
+	private User user;
 
 
 	public static void main(String[] args) {
@@ -47,20 +50,24 @@ public class RestApplication {
 	}
 
 	@GetMapping("/registerUser")
-	public String registerUser(@RequestParam(value = "email", defaultValue = "email") String email) throws JsonParseException, JsonMappingException, IOException {
-		Main m = new Main("1@2345.com", "123", "123");
-		return String.format("user: %s!", m.getCurrentUser());
+	public User registerUser(@RequestParam(value = "email", defaultValue = "email") String email) throws JsonParseException, JsonMappingException, IOException {
+		Main m = new Main("1@234.com", "123", "123");
+		this.user = m.getCurrentUser();
+		return m.getCurrentUser();
+		//String.format("user: %s!", m.getCurrentUser());
 	}
  
 	@GetMapping("/logIn")
-	public String logIn(@RequestParam(value = "email", defaultValue = "email") String email) throws JsonParseException, JsonMappingException, IOException {
+	public User logIn(@RequestParam(value = "email", defaultValue = "email") String email) throws JsonParseException, JsonMappingException, IOException {
 		Main m = new Main("1@2345.com", "123");
-		return String.format("user: %s!", m.getCurrentUser());
+		this.user = m.getCurrentUser();
+		return m.getCurrentUser();
+		//String.format("user: %s!", m.getCurrentUser());
 	}
 
 	//denne metoden funker, men vi trenger noen feilmeldinger inni her et sted
-	@RequestMapping(value="/removeHouse/{location}", method=RequestMethod.GET)
-	public String removeHouse(@PathVariable("location") String location) throws IOException {
+	@ResponseBody @RequestMapping(value="/removeHouse/{location}", method=RequestMethod.GET)
+	public String removeHouse(@PathVariable("location") String location, @RequestBody User user) throws IOException {
 	
 		String l = location.replace('_', ' ');
 
@@ -72,10 +79,30 @@ public class RestApplication {
 			}
 
 			House h = hStream.get(0);
-			String e = h.getUser().getEmail();
-			Jsonworker.removeHouse(l, e);
+
+			if (!h.getUser().equals(user)){
+				return "The house is not registered to this user";
+			}
+
+			Jsonworker.removeHouse(l, user.getEmail());
 			return h.toString() + "is deleted";  
 	}
+
+	@RequestMapping(value = "/house/{location}", method=RequestMethod.GET)
+	public String getHouse(@PathVariable("location") String location) throws IOException {
+
+		String l = location.replace('_', ' ');
+		
+		ArrayList<House> houses = Jsonworker.getAllHouses();
+		List<House> hStream = houses.stream().filter(house -> house.getLocation().equals(l))
+				.collect(Collectors.toList());
+			if (hStream.size() < 1) {
+				return "can not get House";
+			}
+
+			House h = hStream.get(0);
+			return h.toString(); 
+	} 
 
 	
 	@GetMapping("/houses")
@@ -84,17 +111,13 @@ public class RestApplication {
 		return allHouses; 
 	}
 
-
-	/*
-	//funnker ikke
-	@PostMapping(path = "/addHouse", 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE);
-	public ResponseEntity<House> addNewHouse(@RequestBody House newHouse) throws JsonParseException, JsonMappingException, IOException, IllegalArgumentException {
-		House house = 
-		jsonworker.addHouse(location, email);
+	
+	@ResponseBody @RequestMapping("/addHouse/{location}")
+	public String addHouse(@PathVariable("location") String location, @RequestBody User user) throws  IOException{
+		House h = jsonworker.addHouse(location , user.getEmail());
+		return (h.toString() + "is added"); 
 	}
-	*/
+	
 
 
 }
