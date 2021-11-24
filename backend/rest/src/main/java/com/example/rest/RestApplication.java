1b2fe54a79f253802582aaa6b26b9977af4bd961
Hilde Mikaelsen <hildmi@stud.ntnu.no>
2021-11-24 18:27:52 +0000

    changed from get to postmapping

--- backend/rest/src/main/java/com/example/rest/RestApplication.java
+++ backend/rest/src/main/java/com/example/rest/RestApplication.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,14 +34,14 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
-	@GetMapping("/registerUser")
+	@PostMapping("/registerUser")
 	public User registerUser(@RequestBody Map<String, String> request) throws JsonParseException, JsonMappingException, IOException {
 		
 		this.m = new Main(request.get("email"), request.get("password"), request.get("confirmPassword"));
 		return this.m.getCurrentUser();
 	}
  
-	@GetMapping("/logIn")
+	@PostMapping("/logIn")
 	public User logIn(@RequestBody Map<String, String> request) throws JsonParseException, JsonMappingException, IOException {
 		
 		this.m = new Main(request.get("email"), request.get("password"));
