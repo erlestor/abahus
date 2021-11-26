e5495de3921a92c19db889e215d6324213468d13
Erlend Storsve <erlestor@stud.ntnu.no>
2021-11-26 12:06:56 +0100

    show owner of house in housepage

--- frontend/src/components/housePage.js
+++ frontend/src/components/housePage.js
@@ -1,24 +1,47 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 import Image from "../resources/default-house.jpg"
 import "./housePage.css"
 
-const HousePage = ({ houses, user }) => {
+const HousePage = ({ houses, user, fetchHouses }) => {
+  const history = useHistory()
   const { location } = useParams()
   const house = houses.filter((house) => location === house.location)[0]
 
-  const handleDeleteHouse = () => {}
+  const handleDeleteHouse = () => {
+    const requestOptions = {
+      method: "DELETE", // or 'PUT'
+      content: "application/json",
+    }
+    fetch(`http://localhost:8080/removeHouse/${location}`, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response.json()
+      })
+      .then((msg) => {
+        console.log(msg)
+        fetchHouses()
+        // history.push("/")
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+      })
+  }
 
   return (
     <div className="page">
       {house ? (
         <>
-          <h1 className="location">Location</h1>
+          <h1 className="location">{house.location}</h1>
           <img src={Image} width="400px" alt="house" />
-          {house.user.email === user.email && (
+          {house.user === user ? (
             <button className="btn" onClick={handleDeleteHouse}>
               Delete
             </button>
+          ) : (
+            <h1 className="owner">Owner: {house.user}</h1>
           )}
         </>
       ) : (
