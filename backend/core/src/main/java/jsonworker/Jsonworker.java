1fc378ac17b5e85cd047bfae0a1697faeb615a17
Andreas Netteland <andreas@disputas.no>
2021-11-26 16:32:34 +0100

    use portable absolute path

--- backend/core/src/main/java/jsonworker/Jsonworker.java
+++ backend/core/src/main/java/jsonworker/Jsonworker.java
@@ -13,12 +13,9 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SequenceWriter;
 
-
-
-
 public class Jsonworker {
-
-    private static Path dirPath = Paths.get("").toAbsolutePath();
+    private static String userHome = System.getProperty("user.home") + "/gr2129/";
+    private static Path dirPath = Paths.get(userHome).toAbsolutePath();
     private static String housePath = "data/houses.json";
     private static String userPath = "data/users.json";
     private static ObjectMapper mapper = getDefaultObjectMapper();
@@ -27,13 +24,11 @@ public class Jsonworker {
         ObjectMapper defaultObjectMapper = new ObjectMapper();
         return defaultObjectMapper;
     }
-    
 
-    private static <T> ArrayList<T> readFileAsArray (String path_string, Class<T[]> clazz) throws JsonParseException, JsonMappingException, IOException {
+    private static <T> ArrayList<T> readFileAsArray(String path_string, Class<T[]> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
         return new ArrayList<T>(Arrays.asList(mapper.readValue(dirPath.resolve(path_string).toFile(), clazz)));
     }
-
-    
 
     private static <T> void writeFile(String path, ArrayList<T> data) throws IOException {
         File file = new File(dirPath.resolve(path).toString());
@@ -58,7 +53,7 @@ public class Jsonworker {
         }
 
         return null;
-     }
+    }
 
     public static boolean checkPassword(String email, String pwd) throws IOException {
         ArrayList<User> users = getAllUsers();
@@ -67,14 +62,14 @@ public class Jsonworker {
                 return user.getPassword().equals(pwd);
             }
         }
-        return false;        
-     }
+        return false;
+    }
 
-     public static ArrayList<House> getAllHouses() throws JsonParseException, JsonMappingException, IOException {
-         return readFileAsArray(housePath, House[].class);
-     }
-     
-     public static User addUser(String email, String pwd) throws IOException, IllegalArgumentException {
+    public static ArrayList<House> getAllHouses() throws JsonParseException, JsonMappingException, IOException {
+        return readFileAsArray(housePath, House[].class);
+    }
+
+    public static User addUser(String email, String pwd) throws IOException, IllegalArgumentException {
         ArrayList<User> users = getAllUsers();
 
         for (User user : users) {
@@ -83,14 +78,14 @@ public class Jsonworker {
             }
         }
 
-            User user = new User(email, pwd);
-            users.add(user);
-            writeFile(userPath, users);
+        User user = new User(email, pwd);
+        users.add(user);
+        writeFile(userPath, users);
 
-            return user;         
-     }
+        return user;
+    }
 
-     public static User removeUser(String email) throws IOException {
+    public static User removeUser(String email) throws IOException {
         ArrayList<User> users = getAllUsers();
 
         Boolean found = false;
@@ -99,7 +94,7 @@ public class Jsonworker {
         for (User _user : users) {
             if (_user.getEmail().equals(email)) {
                 found = true;
-                user = _user; 
+                user = _user;
             }
         }
         if (!found) {
@@ -108,16 +103,17 @@ public class Jsonworker {
 
         users.remove(user);
         writeFile(userPath, users);
-        return user;         
-     }
+        return user;
+    }
 
-     public static House addHouse(String location, String userEmail) throws JsonParseException, JsonMappingException, IOException, IllegalArgumentException {
+    public static House addHouse(String location, String userEmail)
+            throws JsonParseException, JsonMappingException, IOException, IllegalArgumentException {
         User user;
         try {
-           user = getUser(userEmail);
-            
+            user = getUser(userEmail);
+
         } catch (Exception e) {
-           throw new IllegalArgumentException("user does not exist");
+            throw new IllegalArgumentException("user does not exist");
         }
         ArrayList<House> houses = getAllHouses();
 
@@ -131,9 +127,9 @@ public class Jsonworker {
         houses.add(house);
         writeFile(housePath, houses);
         return house;
-     }
+    }
 
-     public static House removeHouse(String location, String userEmail) throws IOException {
+    public static House removeHouse(String location, String userEmail) throws IOException {
         ArrayList<House> houses = getAllHouses();
         Boolean found = false;
         House house = null;
@@ -141,7 +137,7 @@ public class Jsonworker {
         for (House _house : houses) {
             if (_house.getLocation().equals(location) && _house.getUser().getEmail().equals(userEmail)) {
                 found = true;
-                house = _house; 
+                house = _house;
             }
         }
         if (!found) {
@@ -151,5 +147,5 @@ public class Jsonworker {
         houses.remove(house);
         writeFile(housePath, houses);
         return house;
-     }
+    }
 }
