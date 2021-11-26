125ff2238e91df8cdc9de109c6c102fbf68cf4f6
Erlend Storsve <erlestor@stud.ntnu.no>
2021-11-26 12:03:07 +0100

    connect login and register to api

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,22 +1,61 @@
 import React, { useState } from "react"
 import "./login.css"
+import { useHistory } from "react-router-dom"
 
-const login = () => {
+const login = ({ setUser }) => {
   return (
     <div className="forms-container">
-      <RegisterForm />
-      <LoginForm />
+      <RegisterForm setUser={setUser} />
+      <LoginForm setUser={setUser} />
     </div>
   )
 }
 
-const RegisterForm = () => {
+const RegisterForm = ({ setUser }) => {
+  const history = useHistory()
+
   const [email, setEmail] = useState("")
   const [pass, setPass] = useState("")
   const [confirmPass, setConfirmPass] = useState("")
+  const [error, setError] = useState("")
+
+  const register = () => {
+    const requestOptions = {
+      method: "POST",
+      content: "application/json",
+      body: JSON.stringify({
+        email: email,
+        password: pass,
+        confirmPassword: confirmPass,
+      }),
+    }
+    fetch(`http://localhost:8080/registerUser`, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response.json()
+      })
+      .then((user) => {
+        console.log(user)
+        setError("")
+        setUser(user)
+        setEmail("")
+        setPass("")
+        setConfirmPass("")
+        // store the user in localStorage
+        localStorage.setItem("user", user)
+        // redirect to landing
+        history.push("/")
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+        setError("invalid email/password")
+      })
+  }
 
   return (
-    <form className="registerForm">
+    <div className="registerForm">
       <h1>Register</h1>
       <input
         type="text"
@@ -36,17 +75,53 @@ const RegisterForm = () => {
         value={confirmPass}
         onChange={(e) => setConfirmPass(e.target.value)}
       />
-      <button>Register</button>
-    </form>
+      <button onClick={register}>Register</button>
+      <p>{error}</p>
+    </div>
   )
 }
 
-const LoginForm = () => {
+const LoginForm = ({ setUser }) => {
+  const history = useHistory()
   const [email, setEmail] = useState("")
   const [pass, setPass] = useState("")
+  const [error, setError] = useState("")
+
+  const logIn = () => {
+    const requestOptions = {
+      method: "POST",
+      content: "application/json",
+      body: JSON.stringify({
+        email: email,
+        password: pass,
+      }),
+    }
+    fetch(`http://localhost:8080/logIn`, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response.json()
+      })
+      .then((user) => {
+        console.log(user)
+        setError("")
+        setUser(user)
+        setEmail("")
+        setPass("")
+        // store the user in localStorage
+        localStorage.setItem("user", user)
+        // redirect to landing
+        history.push("/")
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+        setError("invalid email/password")
+      })
+  }
 
   return (
-    <form className="loginForm">
+    <div className="loginForm">
       <h1>Login</h1>
       <input
         type="text"
@@ -60,8 +135,9 @@ const LoginForm = () => {
         value={pass}
         onChange={(e) => setPass(e.target.value)}
       />
-      <button>Log in</button>
-    </form>
+      <button onClick={logIn}>Log in</button>
+      <p>{error}</p>
+    </div>
   )
 }
 
